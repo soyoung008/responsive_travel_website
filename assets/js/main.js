faa238756c89689bdae75f8c7acf1a9bd60f0ff0
devjade <jiyoung.park98@gmail.com>
2021-08-09 16:47:51 +0900

    all section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,13 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
-      /*===== MENU SHOW =====*/
+
+/*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
-  navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu')
-  })
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
 
 /*===== MENU HIDDEN =====*/
@@ -85,3 +86,35 @@ function finalVideo(){
 }
 // ended, when the video ends
 videoFile.addEventListener('ended', finalVideo)
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 200){
+    scrollUp.classList.add('show-scroll');
+  } else{ 
+    scrollUp.classList.remove('show-scroll')
+  }
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
